895a948b790346cad30c32a06bd8bfb27704a1d6
borekmar <83343425+borekmar@users.noreply.github.com>
2021-04-28 12:12:38 +0200

    Add files via upload
    Napisz funkcję alternatywa(x,y), która przyjmuje tylko 1 lub 0
    Napisz funkcję koniunkcja(x,y), która przyjmuje tylko 1 lub 0
    Napisz funkcję implikacja(x,y), która przyjmuje tylko 1 lub 0

---
+++ 08_zadanie7-9.py
@@ -0,0 +1,31 @@
+# Napisz funkcję alternatywa(x,y), która przyjmuje tylko 1 lub 0
+# Napisz funkcję koniunkcja(x,y), która przyjmuje tylko 1 lub 0
+# Napisz funkcję implikacja(x,y), która przyjmuje tylko 1 lub 0
+
+import random
+
+def alternatywa(x, y):
+    if (x == 1 and y == 1) or (x == 1 and y == 0) or (x == 0 and y == 1):
+        return 1
+    elif x == 0 and y == 0:
+        return 0
+
+def koniunkcja(x, y):
+    if x == 1 and y == 1:
+        return 1
+    else:
+        return 0
+
+def implikacja(x, y):
+    if x == 0 and y == 1:
+        return 0
+    else:
+        return 1
+
+p = random.randint(0, 1)
+q = random.randint(0, 1)
+
+print("p:", p, "q:", q)
+print("alternatywa:", alternatywa (p, q))
+print("koniunkcja:", koniunkcja (p, q))
+print("implikacja:", implikacja(p, q))
